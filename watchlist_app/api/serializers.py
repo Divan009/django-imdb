35d7f02a3cdbf1d261d7657c77ab4539b3b52b3d
Diva Dugar <diva.d@crowdanalytix.net>
2021-07-16 20:57:05 +0530

    Added CRUD Functions

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,7 +1,21 @@
 from rest_framework import serializers
-
+from watchlist_app.models import Movie
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
     about = serializers.CharField()
     active = serializers.BooleanField()
+
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        # instance has old value and 
+        # validated_data has new/updated value
+        instance.name = validated_data.get('name', instance.name)
+        instance.about = validated_data.get('about', instance.about)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        return instance
+        
+    
