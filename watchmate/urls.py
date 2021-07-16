751dd0c18b42d65bcf8be9543072b354eb8e25f9
Diva Dugar <diva.d@crowdanalytix.net>
2021-07-16 14:51:09 +0530

    Added Serialization using Django Rest FRamework

--- watchmate/urls.py
+++ watchmate/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
+urlpatterns = [ 
     path('admin/', admin.site.urls),
-    path('movie/', include('watchlist_app.urls')), 
+    path('movie/', include('watchlist_app.api.urls')), 
 ]
