547749f1f783c91c5175a81a7764ee539cc075c0
Divan009 <divadugar@gmail.com>
2021-07-24 15:36:52 +0530

    Added Serializer Fields and Core Arguments

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
+
+def name_length(value):
+    if len(value) < 2:
+        raise serializers.ValidationError("Name is too short")
+    return value
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[name_length])
     about = serializers.CharField()
     active = serializers.BooleanField()
 
@@ -17,5 +22,16 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
-        
-    
+
+# object level validation
+    def validate(self, data):
+        if data['name'] == data['about']:
+            raise serializers.ValidationError("Title and About should be different")
+        return data
+
+# field level validation
+    # def validate_name(self, value):
+    #     if len(value) < 2:
+    #         raise serializers.ValidationError("Name is too short")
+    #     return value
+
